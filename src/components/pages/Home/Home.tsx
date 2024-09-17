c09e7add730e19f605f1fc70230b107b2feee4c3
Jonathan Costa <jonjcosta@gmail.com>
2024-09-17 09:25:29 -0300

    feat: Home component refactored

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,115 +1,128 @@
 import axios from "axios";
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { THE_MEALDB_ENDPOINT } from "../../../utils/Constants";
-import { createMealFromMealApiObject, sortElementListByName } from "../../../utils/StringUtils";
+import { createMealFromMealApiObject, isStringEmpty, sortElementListByName } from "../../../utils/StringUtils";
 import CategoryList from "../../common/CategoryList/CategoryList";
 import LoadingLabel from "../../common/LoadingLabel/LoadingLabel";
 import MealList from "../../common/MealList/MealList";
 import SurpriseMeButton from "../../common/SurpriseMeButton/SurpriseMeButton";
 
-class HomePage extends Component {
+import { FaceFrownIcon } from "@heroicons/react/16/solid";
+import React from 'react';
+import Meal from "../../../models/Meal.model";
 
-    state = {
-        isLoading: false,
-        mealList: [],
-        searchQuery: ''
-    };
+const HomePage: React.FC = () => {
 
-    searchTimer: any = null;
+    const [isSearchingMeals, setIsSearchingMeals] = useState(false);
+    const [isShowCategoryList, setIsShowCategoryList] = useState(true);
+    const [isShowNotFoundMessage, setIsShowNotFoundMessage] = useState(false);
+    const [isShowSurpriseMeButton, setIsShowSurpriseMeButton] = useState(true);
+    const [mealList, setMealList] = useState<Meal[]>([]);
+    const [searchQuery, setSearchQuery] = useState('');
 
-    handleChangeSearch = (event: any) => {
+    var searchTimer: any = null;
+
+    useEffect(() => {
+        const isShowNotFoundMessage = mealList.length === 0 && !isSearchingMeals && !isStringEmpty(searchQuery);
+        const isShowSurpriseMeButton = mealList.length === 0 && !isSearchingMeals && isStringEmpty(searchQuery);
+        const isShowCategoryList = mealList.length === 0 && !isSearchingMeals && isStringEmpty(searchQuery);
+        setIsShowCategoryList(isShowCategoryList);
+        setIsShowSurpriseMeButton(isShowSurpriseMeButton);
+        setIsShowNotFoundMessage(isShowNotFoundMessage);
+
+    }, [isSearchingMeals, mealList, searchQuery]);
+
+    const handleChangeSearch = (event: any) => {
         const query = event.target.value;
-        clearTimeout(this.searchTimer);
+        clearTimeout(searchTimer);
 
-        if (query === '') {
-            this.clearSearch();
+        if (isStringEmpty(query)) {
+            clearSearch();
             return;
         }
 
-        this.searchTimer = setTimeout(() => {
-            this.fetchMealList(query);
+        searchTimer = setTimeout(() => {
+            fetchMealList(query);
         }, 300);
     }
 
-    fetchMealList = async (query: string) => {
+    const fetchMealList = async (query: string) => {
         try {
-            this.setState({ isLoading: true });
+            setSearchQuery(query);
+            setIsSearchingMeals(true);
             const response = await axios.get(`${THE_MEALDB_ENDPOINT}/search.php?s=${query}`);
 
             if (response.data.meals != null) {
-                let mealList = this.generateMealListFromApiResponse(response.data.meals);
+                let mealList = generateMealListFromApiResponse(response.data.meals);
                 mealList = sortElementListByName(mealList);
-
-                this.setState({
-                    isLoading: false,
-                    mealList: mealList,
-                    searchQuery: query
-                });
-
+                setMealList(mealList);
             } else {
-                this.setState({
-                    isLoading: false,
-                    mealList: []
-                });
+                setMealList([]);
             }
+
+            setIsSearchingMeals(false);
 
         } catch (fetchError) {
             console.error(fetchError);
-            this.setState({
-                isLoading: false
-            });
+            setIsSearchingMeals(false);
         }
     }
 
-    generateMealListFromApiResponse(apiMealList: any[]) {
+    const generateMealListFromApiResponse = (apiMealList: any[]) => {
         return apiMealList.map(apiMeal => createMealFromMealApiObject(apiMeal));
     }
 
-    clearSearch = () => {
-        this.setState({
-            mealList: [],
-            searchQuery: ''
-        });
+    const clearSearch = () => {
+        setMealList([]);
+        setSearchQuery('');
     }
 
-    render() {
-        const { mealList, isLoading } = this.state;
-
-        return (
-            <div>
-                <h1 className="text-4xl text-center text-white font-bold font-title
+    return (
+        <div>
+            <h1 className="text-4xl text-center text-white font-bold font-title
                     md:text-5xl lg:text-6xl">
-                    My Meal Book
-                </h1>
-                <h2 className="text-md text-center text-white mt-2 font-semibold font-body
+                My Meal Book
+            </h1>
+            <h2 className="text-md text-center text-white mt-2 font-semibold font-body
                     md:text-lg lg:text-xl">
-                    Looking for delicious meals and recipes?
-                    <br />
-                    We got your back!
-                </h2>
+                Looking for delicious meals and recipes?
+                <br />
+                We got your back!
+            </h2>
 
-                <div className="relative my-6 rounded-md shadow-sm">
-                    <input type="text" name="search" id="search" className="block w-full rounded-md p-3
+            <div className="relative my-6 rounded-md shadow-sm">
+                <input type="text" name="search" id="search" className="block w-full rounded-md p-3
                         text-gray-900 placeholder:text-gray-500 placeholder:text-sm lg:placeholder:text-md"
-                        placeholder="What would you like to eat? :)" autoComplete="off"
-                        onInput={this.handleChangeSearch}></input>
-                </div>
-
-                {(!isLoading && mealList.length === 0) &&
-                    <>
-                        <CategoryList />
-                        <SurpriseMeButton />
-                    </>
-                }
-
-                {isLoading &&
-                    <LoadingLabel labelText="Searching..." />
-                }
-
-                <MealList items={mealList} />
+                    placeholder="What would you like to eat? :)" autoComplete="off"
+                    onInput={handleChangeSearch}></input>
             </div>
-        )
-    }
+
+            {isShowCategoryList &&
+                <CategoryList />
+            }
+
+            {isShowSurpriseMeButton &&
+                <SurpriseMeButton />
+            }
+
+            {isSearchingMeals &&
+                <LoadingLabel labelText={`Searching for '${searchQuery}'...`} />
+            }
+
+            {isShowNotFoundMessage &&
+                <div className="flex flex-row justify-center items-center my-4
+                text-center text-lg text-white font-title">
+                    <p className="text-white">
+                        Sorry, we couldn't find anything...
+                    </p>
+                    &nbsp;
+                    <FaceFrownIcon className="size-6" />
+                </div>
+            }
+
+            <MealList items={mealList} />
+        </div>
+    )
 }
 
 export default HomePage;
